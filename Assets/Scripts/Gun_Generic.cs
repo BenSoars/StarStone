4df38f05465896c98fc631e76e3b2e71770bee48
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-06-08 15:17:49 +0100

    Gun Updated

--- Assets/Scripts/Gun_Generic.cs
+++ Assets/Scripts/Gun_Generic.cs
@@ -6,13 +6,28 @@ public class Gun_Generic : MonoBehaviour
 {
 
     // Ben Soars
-
+    [Header("Weapon Ammo")]
+    [Space (2)]
     public int m_maxAmmo = 6; // the maximum ammo
     public int m_currentAmmo = 6; // the current ammo
     public int m_ammoPerShot = 1; // how much ammo shooting will consume
-    public int m_bulletDamage = 1;
-    public float m_shotForce = 100; // how much force the bullet will be shot for, it hitscan leave blank
 
+    [Space(2)]
+    [Header("Weapon Accuracy")]
+    [Space(2)]
+    public bool m_isAccurate = true;
+    public Vector3 m_accuracyMinRange;
+    public Vector3 m_accuracyMaxRange;
+    
+    [Space(2)]
+    [Header("Weapon Other Stats")]
+    [Space(2)]
+    public float m_shotForce = 100; // how much force the bullet will be shot for, it hitscan leave blank
+    public int m_bulletDamage = 1;
+
+    [Space(2)]
+    [Header("Shot Type")]
+    [Space(2)]
     public Rigidbody m_physicalBullet; // leave this blank if the gun is hitscan
     public Transform m_shotPoint; // the point where the bullet is shot from
     private Rigidbody m_shotBullet;
@@ -22,25 +37,25 @@ public class Gun_Generic : MonoBehaviour
 
     void f_ShootGun()
     {
-        if (m_physicalBullet) // if a physical bullet is there
+        for (int i = 0; i < m_ammoPerShot; i++)
         {
-            m_shotBullet = Instantiate(m_physicalBullet, m_shotPoint.transform.position, Quaternion.identity) as Rigidbody; // shoot bullet
-            m_shotBullet.AddForce (m_shotPoint.transform.forward * m_shotForce); // push bullet out
-            
-        } else {
-
-            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out m_hitscanCast, Mathf.Infinity)) // shoot out a raycast for hitscan
+            if (m_physicalBullet) // if a physical bullet is there
             {
-                // if hit something
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * m_hitscanCast.distance, Color.yellow);
-                Debug.Log("Did Hit");
-                Instantiate(hitSpark, m_hitscanCast.point, Quaternion.identity);
+                m_shotBullet = Instantiate(m_physicalBullet, m_shotPoint.transform.position, Quaternion.identity) as Rigidbody; // shoot bullet
+                m_shotBullet.AddForce(m_shotPoint.transform.forward * m_shotForce); // push bullet out
+
             }
             else
             {
-                // if missed
-                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-                Debug.Log("Did not Hit");
+
+                if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out m_hitscanCast, Mathf.Infinity)) // shoot out a raycast for hitscan
+                {
+                    // if hit something
+                    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * m_hitscanCast.distance, Color.yellow);
+                    Debug.Log("Did Hit");
+                    Instantiate(hitSpark, m_hitscanCast.point, Quaternion.identity);
+
+                }
             }
         }
 
