ceb0139a7f1e991b80d31b0a30f1dc951d77d43d
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-06-15 14:57:06 +0100

    Added Early Wave System to TempleTest
    - No 2d Array
    -

--- Assets/Scripts/Wave_System.cs
+++ Assets/Scripts/Wave_System.cs
@@ -7,17 +7,42 @@ public class Wave_System : MonoBehaviour
     public List<GameObject> enemyTypes = new List<GameObject>();
     public List<Transform> spawnPoints = new List<Transform>();
 
-    public List<List<int>> waves = new List<List<int>>();
+    public List<int> amountOf = new List<int>();
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+    public int enemiesLeft;
 
-    // Start is called before the first frame update
-    void Start()
+    private int curRound;
+
+    void SpawnEnemies()
     {
-        
+        for (int i = 0; i < amountOf[curRound]; i++)
+        {
+            int rando = Random.Range(0, spawnPoints.Count);
+            GameObject spawned = Instantiate(enemyTypes[0], spawnPoints[rando].position, Quaternion.identity);
+            spawnedEnemies.Add(spawned);
+        }
+        curRound += 1;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        enemiesLeft = spawnedEnemies.Count;
+
+        if (spawnedEnemies.Count <= 0)
+        {
+            SpawnEnemies();
+        }
+        else
+        {
+            for (int i = 0; i < spawnedEnemies.Count; i++)
+            {
+                if (spawnedEnemies[i] == null)
+                {
+                    spawnedEnemies.RemoveAt(i);
+                    break;
+                }
+            }
+        }
     }
 }
