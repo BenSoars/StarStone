b77577584aa8e5756e42e653e3a3bef9cfb5cccd
WatsonK-Dev <WatsonK-Dev@users.noreply.github.com>
2020-06-19 15:13:21 +0100

    Starstone Classes

--- Assets/Scripts/Gun_Prototype.cs
+++ Assets/Scripts/Gun_Prototype.cs
@@ -1,18 +1,121 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Gun_Prototype : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private LineRenderer m_lr;
+
+    public Transform m_shotPoint;
+
+    public GameObject m_hitDamageText;
+
+    public float m_laserDamage;
+
+    public int m_classState;
+
+    public float m_coolDown;
+    private float m_currentCooldown;
+
+
+    // Use this for initialization
     void Start()
     {
-        
+        m_currentCooldown = m_coolDown;
+
+        m_classState = 1;
+        m_lr = GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
+    [System.Obsolete]
     void Update()
     {
-        
+        f_startstoneSelect();
+        f_prototypeClasses();
+
+        m_currentCooldown -= Time.deltaTime;
+        m_laserDamage = Random.Range(5, 10);
+    }
+
+    void f_startstoneSelect()
+    {
+        RaycastHit m_stoneSelect;
+
+        if (Physics.Raycast(m_shotPoint.position, m_shotPoint.forward, out m_stoneSelect, 2f, 1 << 11))
+        {
+            switch (m_stoneSelect.collider.gameObject.name)
+            {
+                case ("Starstone 1"):
+                    m_classState = 0;
+                    break;
+                case ("Starstone 2"):
+                    m_classState = 1;
+                    break;
+                case ("Starstone 3"):
+                    m_classState = 2;
+                    break;
+                case ("Starstone 4"):
+                    m_classState = 3;
+                    break;
+            }
+        }
+    }
+
+    [System.Obsolete]
+    void f_prototypeClasses()
+    {
+        RaycastHit m_laserHit;
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            m_lr.SetPosition(0, transform.position);
+            m_lr.enabled = true;
+            if (Physics.Raycast(m_shotPoint.position, m_shotPoint.forward, out m_laserHit))
+            {
+                if (m_laserHit.collider)
+                {
+                    m_lr.SetPosition(1, m_laserHit.point);
+                }
+
+                if (m_laserHit.collider.gameObject.CompareTag("Enemy") && m_currentCooldown <= 0)
+                {
+                    switch (m_classState)
+                    {
+                        case 0:
+                            m_laserHit.collider.gameObject.GetComponent<Enemy_Controller>().m_enemyHealth -= m_laserDamage;
+                            Instantiate(m_hitDamageText, m_laserHit.point, Quaternion.identity);
+                            m_currentCooldown = m_coolDown;
+                            break;
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                    }
+                }
+            }
+        }
+        else
+        {
+            m_lr.enabled = false;
+        }
+
+        switch (m_classState)
+        {
+            case 0:
+                m_lr.SetColors(Color.red, Color.red);
+                break;
+            case 1:
+                m_lr.SetColors(Color.yellow, Color.yellow);
+                break;
+            case 2:
+                m_lr.SetColors(Color.blue, Color.blue);
+                break;
+            case 3:
+                m_lr.SetColors(Color.green, Color.green);
+                break;
+        }
     }
 }
--- Assets/Scripts/Text_HitDamage.cs
+++ Assets/Scripts/Text_HitDamage.cs
@@ -7,17 +7,40 @@ public class Text_HitDamage : MonoBehaviour
 {
     private Player_Controller r_playerController;
     private Gun_Generic r_gunGeneric;
+    private Gun_Prototype r_gunPrototype;
+
+    private float m_bulletDamage;
+
+    private bool m_textUpdated;
     // Start is called before the first frame update
     void Start()
     {
         r_playerController = FindObjectOfType<Player_Controller>();
         r_gunGeneric = FindObjectOfType<Gun_Generic>();
-        GetComponentInChildren<TextMeshPro>().text = "" + r_gunGeneric.m_bulletDamage;
+        r_gunPrototype = FindObjectOfType<Gun_Prototype>();       
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {     
+        switch (r_gunPrototype.m_classState)
+        {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+                m_bulletDamage = r_gunPrototype.m_laserDamage;
+                break;
+            default:
+                m_bulletDamage = r_gunGeneric.m_bulletDamage;
+                break;
+        }  
+
+        if (m_textUpdated == false)
+        { 
+            GetComponentInChildren<TextMeshPro>().text = "" + m_bulletDamage;
+            m_textUpdated = true;
+        }
 
         float m_distance = Vector3.Distance(transform.position, r_playerController.transform.position);
 
