80d7c72e488ef5aedac5803b761b3265d7125ea2
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-06-24 14:52:49 +0100

    Added Notes system
    ESC - Open Notes
    X Next Note
    Z Last Note

---
+++ Assets/Inventory.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+
+    public List<string> noteName = new List<string>();
+    public List<string> noteContents = new List<string>();
+    public List<Sprite> noteImage = new List<Sprite>();
+    private bool m_isVisible;
+    private int currentNote;
+    public GameObject notesVisible;
+
+    public Text m_noteNameDisplay;
+    public Text m_noteContentsDisplay;
+    public Image m_noteImageDisplay;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            m_isVisible = !m_isVisible;
+        }
+
+        notesVisible.SetActive(m_isVisible);
+
+        if (m_isVisible && noteName.Count > 0)
+        {
+            m_noteNameDisplay.text = noteName[currentNote];
+            m_noteContentsDisplay.text = noteContents[currentNote];
+            m_noteImageDisplay.sprite = noteImage[currentNote];
+
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                currentNote += 1;
+                if (currentNote > noteName.Count)
+                {
+                    currentNote = 0;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                currentNote -= 1;
+                if (currentNote < 0)
+                {
+                    currentNote = noteName.Count - 1;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Note.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour
+{
+
+    public string NoteName;
+    public string NoteText;
+    public Sprite NoteImage;
+
+    private Inventory m_invSystem;
+
+    void Start()
+    {
+        m_invSystem = GameObject.FindObjectOfType<Inventory>();
+    }
+
+    // Start is called before the first frame update
+    void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            pickUpNote();
+        }
+    }
+
+
+    void pickUpNote()
+    {
+        m_invSystem.noteName.Add(NoteName);
+        m_invSystem.noteContents.Add(NoteText);
+        m_invSystem.noteImage.Add(NoteImage);
+        Destroy(gameObject);
+    }
+}
