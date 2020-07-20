dc8e879fc3cd466d0f2df2dfcfe610854dba0028
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-07-20 16:26:34 +0100

    Added Lighing Pass, and a few other smaller things

--- Assets/Scripts/Options.cs
+++ Assets/Scripts/Options.cs
@@ -7,6 +7,7 @@ public class Options : MonoBehaviour
 {
     // Ben Soars
     public Slider volumeSlider;
+    public AudioSource testSound; // the test sound that plays when altering volume
 
     void Start()
     {
@@ -15,17 +16,19 @@ public class Options : MonoBehaviour
     }
 
     void FirstTime() {
-        if (PlayerPrefs.GetFloat("volumeLevel") == 0)
+        if (PlayerPrefs.GetFloat("volumeLevel") == 0) // if this is the player's first time playing, set volume to a decent level
         {
             PlayerPrefs.SetFloat("volumeLevel", 0.8f);
-            PlayerPrefs.Save();
+            PlayerPrefs.Save(); 
         }
        
     }
 
     public void alterSoundVolume()
     {
-        PlayerPrefs.SetFloat("volumeLevel", volumeSlider.value);
-        PlayerPrefs.Save();
+        PlayerPrefs.SetFloat("volumeLevel", volumeSlider.value); // set volume to reflect the slider
+        testSound.volume = volumeSlider.value; // set test sound to use that volume
+        testSound.Play(); // play the test sound
+        PlayerPrefs.Save(); // save the volume level
     }
 }
