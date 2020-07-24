a4c49baeba9d692de1e091c5a93b8cdd15a9d603
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-07-24 16:40:35 +0100

    Commented more of my Code

---
+++ Assets/Scripts/DisplayAchivements.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayAchivements : MonoBehaviour
+{
+    // Ben Soars
+    public List<GameObject> allAchivements = new List<GameObject>(); // list of all achivement places
+    private AchivementTracker m_achivement; // the achivement tracker
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1; // reset the time scale
+        Invoke("CheckAll", 0.1f); //wait a frame before checking, just in case there are multiple achivement trackers in the scene
+    }
+
+    public void CheckAll()
+    {
+        m_achivement = GameObject.FindObjectOfType<AchivementTracker>(); // get the achivement tracker
+        for (int i = 0; i < m_achivement.unlockImages.Count; i++) // for loop through all the achivements listed in the tracker
+        {
+            displayAchivements(i); // display the achivement and pass through i
+        }
+    }
+
+    private void displayAchivements(int achivementID) // display achivement at passed position
+    {
+        Image thumbnail = allAchivements[achivementID].transform.GetChild(2).GetComponent<Image>(); // get the image component from the current achivement display area
+        allAchivements[achivementID].transform.GetChild(1).GetComponent<Text>().text = m_achivement.unlockNames[achivementID]; // set the text to match the current achivement ID
+        thumbnail.sprite = m_achivement.unlockImages[achivementID]; // set the achivement image to match the current ID
+        allAchivements[achivementID].transform.GetChild(3).GetComponent<Text>().text = m_achivement.unlockDesc[achivementID]; // update the description of the current achivement ID
+        if (PlayerPrefs.GetInt(m_achivement.unlockNames[achivementID]) == 0) // if the passed variable isn't already unlocked
+        {
+            thumbnail.color = Color.grey; // set the achivement to be greyed out
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/UnlockAchivement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockAchivement : MonoBehaviour // used to unlock an achivement upon entering a secene or by enabling a certain gameobject
+{
+    public int AchivementID; // the ID of the achivement that
+    private AchivementTracker m_achivement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_achivement = GameObject.FindObjectOfType<AchivementTracker>(); //get the achivement tracker system
+        m_achivement.UnlockAchivement(AchivementID); //unlock the passed achivement from the start
+    }
+}
