e2073137222a5ae458355d267e66e61b4313b11b
BenSoars <61790906+BenSoars@users.noreply.github.com>
2020-09-07 17:03:17 +0100

    Additional Sound Effects

---
+++ Assets/Scripts/Effect_Volume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect_Volume : MonoBehaviour
+{
+    public AudioSource sound; // used for the music
+    // Start is called before the first frame update
+    void Start()
+    {
+        fixVolume();
+    }
+
+    public void fixVolume()
+    {
+        sound.volume = PlayerPrefs.GetFloat("volumeLevel"); // set the music level
+        sound.Play(); // play the musi
+
+
+    }
+}
